4dc4d2d5763f43033dc6962c01d46002aeb3359b
Macintyre Sunde <imacsunde@gmail.com>
2022-11-08 21:18:26 -0500

    staged

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import bico_scheduling as bs
 
 
-# d = bs.bmc.get_data_list_of_dicts("brynmawr/data/Fall2000.csv")
-# bs.bmc.write_constraints_to_file(d, "test/constraints.txt")
-# bs.bmc.write_prefs_to_file(d, "test/prefs.txt")
+d = bs.bmc.get_data_list_of_dicts("brynmawr/data/Fall2000.csv")
+bs.bmc.write_constraints_to_file(d, "test/constraints.txt")
+bs.bmc.write_prefs_to_file(d, "test/prefs.txt")
 
 students, classes, rooms, times, profs = bs.prep_data(
-    "basic\demo_constraints.txt", "basic\demo_studentprefs.txt")
+    "test\constraints.txt", "test/prefs.txt")
 
 
 schedule = bs.make_schedule(students, classes, rooms, times, profs)
