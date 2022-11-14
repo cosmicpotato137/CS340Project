06d7635cacf721d1bd610a908c694ca6dbb23496
Macintyre Sunde <imacsunde@gmail.com>
2022-11-13 21:31:33 -0500

    final make schedule.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import bico_scheduling as bs
 
-d = bs.bmc.get_data_list_of_dicts("brynmawr/data/Fall2001.csv")
+d = bs.bmc.get_data_list_of_dicts("brynmawr/data/Fall2007.csv")
 bs.bmc.write_constraints_to_file(d, "test/bconstraints.txt")
 bs.bmc.write_prefs_to_file(d, "test/bprefs.txt")
 
@@ -8,15 +8,18 @@ d = bs.hav.get_data_list_of_dicts("haverford\haverfordEnrollmentDataS14.csv")
 bs.hav.write_constraints_to_file(d, "test/hconstraints.txt")
 bs.hav.write_prefs_to_file(d, "test/hprefs.txt")
 
+# students, classes, rooms, times, profs = bs.prep_data(
+#     "test/bconstraints.txt", "test/bprefs.txt", "test/hconstraints.txt", "test/hprefs.txt")
+
 students, classes, rooms, times, profs = bs.prep_data(
-    "test/bconstraints.txt", "test/bprefs.txt", "test/hconstraints.txt", "test/hprefs.txt")
+    "test/hconstraints.txt", "test/hprefs.txt")
 
 # students, classes, rooms, times, profs = bs.prep_data(
-#     "test/hconstraints.txt", "test/hprefs.txt")
+#     "test/bconstraints.txt", "test/bprefs.txt")
 
 class_p = bs.class_priority(classes)
 student_p = bs.student_priority(students)
-student_athletes = bs.student_athletes(students, .2)
+student_athletes = bs.student_athletes(students, .386)
 
 custom_times = bs.custom_times(8, 16, 1)
 schedule, reg_students = bs.make_schedule(
